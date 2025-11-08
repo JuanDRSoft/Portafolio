167dd8066078da24fdfc1f78a8724b56ce2a1681
Juan Diego Rios <juandirio2011@gmail.com>
2025-11-07 23:04:56 -0500

    feat: profile img

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,22 @@
 import { scrollToSection } from '@/utils/functions'
 import { Github, Linkedin, Mail } from 'lucide-react'
+import Image from 'next/image'
+import PROFILE from '../public/profile.jpg'
 
 const Hero = () => {
   return (
     <section id="hero" className="min-h-screen flex items-center justify-center px-6 pt-20">
       <div className="max-w-4xl text-center">
-        <div className="w-32 h-32 bg-gradient-to-br from-gray-800 to-gray-600 rounded-full mx-auto mb-8 flex items-center justify-center text-white text-4xl font-bold opacity-0 animate-fadeIn">
-          TN
-        </div>
+        <Image
+          src={PROFILE}
+          alt="Logo"
+          width={100}
+          height={100}
+          className="w-32 h-32 shadow-xl border border-gray-200  bg-gradient-to-br from-gray-800 to-gray-600 rounded-full mx-auto mb-8 flex items-center justify-center text-white text-4xl font-bold opacity-0 animate-fadeIn"
+        />
+
         <h1 className="text-5xl md:text-7xl font-bold mb-6 opacity-0 animate-fadeInUp delay-200">
-          Full Stack Developer
+          Software Developer
         </h1>
         <p className="text-xl md:text-2xl text-gray-600 mb-8 opacity-0 animate-fadeInUp delay-300">
           Creando experiencias web excepcionales con código limpio y diseño moderno
